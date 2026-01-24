a60820767361ff94178c4a1dc64deb925f185643
Sushant <sushantbhagat561@gmail.com>
2026-01-24 10:16:49 +0530

    create loop to generate color thems, relocate commands codes

--- main.py
+++ main.py
@@ -18,11 +18,6 @@ save_as_icon = tk.PhotoImage(file = "icons2/save_as.png")
 exit_icon = tk.PhotoImage(file = "icons2/exit.png")
 
 file = tk.Menu(main_menu, tearoff = 0)
-file.add_command(label = "New", image = new_icon, compound = tk.LEFT, accelerator = "Ctrl+N")
-file.add_command(label = "Open", image = open_icon, compound = tk.LEFT, accelerator = "Ctrl+O")
-file.add_command(label = "Save", image = save_icon, compound = tk.LEFT, accelerator = "Ctrl+S")
-file.add_command(label = "Save As", image = save_as_icon, compound = tk.LEFT, accelerator = 'Ctrl+Alt+S')
-file.add_command(label = "Exit", image = exit_icon, compound = tk.LEFT, accelerator = "Ctrl+Q")
 
 #Edit menu
 copy_icon = tk.PhotoImage(file = "icons2/copy.png")
@@ -32,19 +27,12 @@ clear_all_icon = tk.PhotoImage(file = 'icons2/clear_all.png')
 find_icon = tk.PhotoImage(file = 'icons2/find.png')
 
 edit = tk.Menu(main_menu, tearoff = 0)
-edit.add_command(label = "Copy", image = copy_icon, compound = tk.LEFT, accelerator = "Ctrl+C")
-edit.add_command(label = "Paste", image = paste_icon, compound = tk.LEFT, accelerator = "Ctrl+Y")
-edit.add_command(label = "Cut", image = cut_icon, compound = tk.LEFT, accelerator = 'Ctrl+X')
-edit.add_command(label = "Clear All", image = clear_all_icon, compound = tk.LEFT, accelerator = "Ctrl+Alt+X")
-edit.add_command(label = "Find", image = find_icon, compound = tk.LEFT, accelerator = "Ctrl+F")
 
 #view menu
 tool_bar_icon = tk.PhotoImage(file = "icons2/tool_bar.png")
 status_bar_icon = tk.PhotoImage(file = "icons2/status_bar.png")
 
 view = tk.Menu(main_menu, tearoff = 0)
-view.add_checkbutton(label = "Tool Bar", image = tool_bar_icon, compound = tk.LEFT)
-view.add_checkbutton(label = "Status Bar", image = status_bar_icon, compound = tk.LEFT)
 
 #color menu
 light_default_icon = tk.PhotoImage(file = "icons2/light_default.png")
@@ -55,12 +43,18 @@ monokai_icon = tk.PhotoImage(file = "icons2/monokai.png")
 night_blue_icon = tk.PhotoImage(file = "icons2/night_blue.png")
 
 color_theme = tk.Menu(main_menu, tearoff = 0)
-color_theme.add_checkbutton(label = "Light Default", image = light_default_icon, compound = tk.LEFT)
-color_theme.add_checkbutton(label = "Light Plus", image = light_plus_icon, compound = tk.LEFT)
-color_theme.add_checkbutton(label = "Dark", image = dark_icon, compound = tk.LEFT)
-color_theme.add_checkbutton(label = "Red", image = red_icon, compound = tk.LEFT)
-color_theme.add_checkbutton(label = "Monokai", image = monokai_icon, compound = tk.LEFT)
-color_theme.add_checkbutton(label = "Night Blue", image = night_blue_icon, compound = tk.LEFT)
+
+theme_choice = tk.StringVar()
+color_icons = (light_default_icon, light_plus_icon, dark_icon, red_icon, monokai_icon, night_blue_icon)
+
+color_dict = {
+    'Light Default': ('#000000', '#ffffff'),
+    'Light Plus': ('#474747', '#e0e0e0'),
+    'Dark': ('#c4c4c4', '#2d2d2d'),
+    'Red': ('#2d2d2d', '#ffe8e8'),
+    'Monokai': ('#d3b774', '#474747'),
+    'Night Blue': ('#ededed', '#6b9dc2')
+}
 
 #cascade
 main_menu.add_cascade(label = "File", menu = file)
@@ -79,8 +73,33 @@ main_menu.add_cascade(label = "Color", menu = color_theme)
 # ----------------------- main statusbar coding -----------------------
 # ----------------------- main statusbar conding End -----------------------
 
-# ----------------------- Main Menu function -----------------------
-# ----------------------- Main Menu function End -----------------------
+# ----------------------- Main Menu functionality -----------------------
+#file commands
+file.add_command(label = "New", image = new_icon, compound = tk.LEFT, accelerator = "Ctrl+N")
+file.add_command(label = "Open", image = open_icon, compound = tk.LEFT, accelerator = "Ctrl+O")
+file.add_command(label = "Save", image = save_icon, compound = tk.LEFT, accelerator = "Ctrl+S")
+file.add_command(label = "Save As", image = save_as_icon, compound = tk.LEFT, accelerator = 'Ctrl+Alt+S')
+file.add_command(label = "Exit", image = exit_icon, compound = tk.LEFT, accelerator = "Ctrl+Q")
+
+#edit commands
+edit.add_command(label = "Copy", image = copy_icon, compound = tk.LEFT, accelerator = "Ctrl+C")
+edit.add_command(label = "Paste", image = paste_icon, compound = tk.LEFT, accelerator = "Ctrl+Y")
+edit.add_command(label = "Cut", image = cut_icon, compound = tk.LEFT, accelerator = 'Ctrl+X')
+edit.add_command(label = "Clear All", image = clear_all_icon, compound = tk.LEFT, accelerator = "Ctrl+Alt+X")
+edit.add_command(label = "Find", image = find_icon, compound = tk.LEFT, accelerator = "Ctrl+F")
+
+#views commands
+view.add_checkbutton(label = "Tool Bar", image = tool_bar_icon, compound = tk.LEFT)
+view.add_checkbutton(label = "Status Bar", image = status_bar_icon, compound = tk.LEFT)
+
+#color theme commands
+count = 0
+for i in color_dict:
+    color_theme.add_radiobutton(label = i, image = color_icons[count], compound=tk.LEFT, variable = theme_choice)
+    count += 1
+
+
+# ----------------------- Main Menu functionality End -----------------------
 
 
 
